abcb02b67f3453fc2f4436db6d198a653454092c
Erno Szabados <erno.szabados@windowslive.com>
2025-05-04 21:04:29 +0200

    Fix library dependency flags. Remove unused code.

--- src/llm_util.c
+++ src/llm_util.c
@@ -1,14 +1,6 @@
 
 #include "llm_util.h"
 
-/// @brief  Safely duplicate a string
-/// @param str 
-/// @return 
-gchar *safe_strdup(const gchar *str)
-{
-    return str ? g_strdup(str) : NULL;
-}
-
 
 /// @brief Construct server URI from base uri and path
 gchar* llm_construct_server_uri_string(const gchar* server_base_uri, const gchar *path)
--- src/llm_util.h
+++ src/llm_util.h
@@ -3,8 +3,6 @@
 
 #include <glib.h>
 
-gchar *safe_strdup(const gchar *str);
-
 gchar* llm_construct_server_uri_string(const gchar* server_base_uri, const gchar *path);
 
 #endif // __LLM_UTIL_H__
